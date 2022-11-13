cd53d283af127cec9bd78983ec5a12e9627735a8
Ananya Garg <agarg.ananya@gmail.com>
2022-11-12 23:19:06 -0500

    image of cat

--- cat-facts/src/App.js
+++ cat-facts/src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
      <h1>Cat Facts Generator</h1>
+     <img src="https://media.istockphoto.com/photos/many-cats-sitting-in-a-row-isolated-on-white-picture-id889499124?k=20&m=889499124&s=612x612&w=0&h=ujaZadNxcS4RQl3V9r0OZxxZa0wWDCctuw2vUKHBZeA=" height="50"></img>
      <CatFacts/>
     </div>
   );
