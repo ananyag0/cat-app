d8fc9136ed4764a183a3257c15b724120ba0458b
Ananya Garg <agarg.ananya@gmail.com>
2022-11-12 20:18:52 -0500

    adding previously generated facts button

--- cat-facts/src/App.js
+++ cat-facts/src/App.js
@@ -14,30 +14,4 @@ function App() {
 
 export default App;
 
-// function CatFacts() {
-//   const [facts, setFacts] = useState([]);
-//   const fetchData = async () => {
-//     const response = await fetch("https://catfact.ninja/fact");
-//     const data = await response.json();
-//     setFacts(data);
-//     const printOut = data.fact;
-//     // console.log(facts);
-//     var array = {...facts, data.facts};
-//    console.log(data);
 
-//   };
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-//   return (
-    
-//     <div className = 'container'>
-//       <p id = "fact"> {facts.fact} </p>
-//       <button className = "button" onClick = {fetchData}> Give me a fact </button>
-//     </div>
-//   );
-// }
-
-
-// export default CatFacts;
--- cat-facts/src/CatFacts.js
+++ cat-facts/src/CatFacts.js
@@ -2,17 +2,20 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function CatFacts() {
-  const [facts, setFacts] = useState([]);
-  const fetchData = async () => {
-    const response = await fetch("https://catfact.ninja/fact");
-    const data = await response.json();
-    setFacts(data);
-    const printOut = data.fact;
-    // console.log(facts);
-    // var array = {...facts, data.facts};
-   console.log(data);
-
-  };
+    const [facts, setFacts] = useState([]);
+    const [pFacts, setPFacts] = useState([]);
+    const [fav, setFav] = useState(false);
+    const [counter, setCounter] = useState(0);
+    const fetchData = async () => {
+        const response = await fetch("https://catfact.ninja/fact");
+        const data = await response.json();
+        if (facts != null && facts.fact != null) {
+            pFacts.unshift({favorite: fav, fact: facts.fact});
+            setFav(false);
+        }
+        setPFacts(pFacts);
+        setFacts(data);
+    };
 
   useEffect(() => {
     fetchData();
@@ -21,10 +24,11 @@ function CatFacts() {
     
     <div className = 'container'>
       <p id = "fact"> {facts.fact} </p>
-      <button className = "button" onClick = {fetchData}> Give me a fact </button>
+      <button className = "button" onClick = {fetchData}> Give me a fact! </button>
+      <p button = "facts-list">Here are your previously generated facts: </p>
     </div>
   );
-}
 
+  }
 
-export default CatFacts;
+  export default CatFacts;
